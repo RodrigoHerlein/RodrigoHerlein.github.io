5042ccebbd38809cacebcc43ba6bc5c3bc357730
RodrigoHerlein <34068521+RodrigoHerlein@users.noreply.github.com>
2023-09-25 18:52:49 -0300

    Update MyElemente.js

--- MyElemente.js
+++ MyElemente.js
@@ -2,7 +2,7 @@ class MiComponente extends HTMLElement {
 
 constructor(){
 	super();
-	this.innerHTML = '<p>No se que pelicula poner</p>';
+	this.innerHTML = '<p2012</p>';
 }
 }
 customElements.define("team-e",MiComponente);
